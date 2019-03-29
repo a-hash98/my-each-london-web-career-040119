fc36e2be655b44a981703ba777d02f5d7d0f0689
Ay Has <ahashim983@yahoo.com>
2019-03-29 01:22:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,10 +5,11 @@ def my_each(array) # put argument(s) here
     while i < array.length-1
       yield[i]
       i += 1
+    end
   else
     puts "No block was given!"
   end
-end
+ends
 
 my_each(array) do |item|
   puts item
