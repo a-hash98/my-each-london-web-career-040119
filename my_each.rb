6bbb0ea87ff378671b52a41d21a78ddb4c5b8c9a
Ay Has <ahashim983@yahoo.com>
2019-03-29 01:21:34 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,13 @@
 def my_each(array) # put argument(s) here
   # code here
-  i = 0
-  while i < array.length-1
-    yield[i]
-    i += 1
+  if block_given?
+    i = 0
+    while i < array.length-1
+      yield[i]
+      i += 1
+  else
+    puts "No block was given!"
+  end
 end
 
 my_each(array) do |item|
