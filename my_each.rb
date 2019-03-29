2b96040ee6077c2c0175454384d60b16768bb1cd
Ay Has <ahashim983@yahoo.com>
2019-03-29 01:28:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(array) # put argument(s) here
   # code here
   if block_given?
     i = 0
-    while i < array.length-1
+    while i < array.length
       yield array[i]
       i += 1
     end
